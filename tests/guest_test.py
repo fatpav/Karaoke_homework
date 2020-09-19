9e3bf84f3bda527cf1ea0a79c29411ff5f85f3bf
fatpav <fat_pav@hotmail.com>
2020-09-19 14:50:06 +0100

    first commit

---
+++ tests/guest_test.py
@@ -0,0 +1,6 @@
+import unittest
+
+from src.room import Room
+from src.guest import Guest
+from src.song import song
+
---
+++ tests/song_test.py
@@ -0,0 +1,5 @@
+import unittest
+
+from src.room import Room
+from src.guest import Guest
+from src.song import song
